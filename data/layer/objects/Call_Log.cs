d85618c2d485e1d1cdeb93c865664af27b62311a
Etienne de Villiers <etiennefdevilliers@gmail.com>
2021-03-18 17:57:37 +0200

    Access modifier for Call_Log public

--- data/layer/objects/Call_Log.cs
+++ data/layer/objects/Call_Log.cs
@@ -5,7 +5,7 @@ using System.Text;
 namespace sen381_t7_premier_service_solutions.data.layer.objects
 {
     
-    class Call_Log
+    public class Call_Log
     {
         public DateTime dtCallStarted;
         public DateTime dtCallEnded;
