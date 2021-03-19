3df5f0258125aeb33f419f3173fe3c782a40f057
RubenDebeer <boepsman@gmail.com>
2021-03-19 09:18:59 +0200

    AddesServices

---
+++ data/layer/objects/ServiceContract/Service_Contract.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace sen381_t7_premier_service_solutions
+{
+    class Service_Contract
+    {
+        //Fields
+        private int ID;
+        private string sc_description;
+        private List<Tuple<Package, Service_Level_Agreement>> service_list;
+        private double cost;
+        private DateTime date_valid_start;
+        private DateTime date_valid_end; 
+        private string indended_for;
+
+        //Properties
+        public int ID1 { get => ID; set => ID = value; }
+        public string Sc_description { get => sc_description; set => sc_description = value; }
+        public double Cost { get => cost; set => cost = value; }
+        public DateTime Date_valid_start { get => date_valid_start; set => date_valid_start = value; }
+        public DateTime Date_valid_end { get => date_valid_end; set => date_valid_end = value; }
+        public string Indended_for { get => indended_for; set => indended_for = value; }
+        internal List<Tuple<Package, Service_Level_Agreement>> Service_list { get => service_list; set => service_list = value; }
+         
+        //Custom Constructor
+        public Service_Contract(int iD1, string sc_description, double cost, DateTime date_valid_start, DateTime date_valid_end, string indended_for, List<Tuple<Package, Service_Level_Agreement>> service_list)
+        {
+            ID1 = iD1;
+            Sc_description = sc_description;
+            Cost = cost;
+            Date_valid_start = date_valid_start;
+            Date_valid_end = date_valid_end;
+            Indended_for = indended_for;
+            Service_list = service_list;
+        }
+
+        public override bool Equals(object obj)
+        {
+
+            if ((obj == null) || !this.GetType().Equals(obj.GetType()))
+            {
+                return false;
+            }
+            else
+            {
+                Service_Contract p = (Service_Contract)obj;
+                return p.ID.Equals(this.ID);
+            }
+
+        }
+
+        public override int GetHashCode()
+        {
+            return this.ID;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+    }    
+}
---
+++ data/layer/objects/ServiceContract/Service_Level_Agreement.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace sen381_t7_premier_service_solutions
+{
+    class Service_Level_Agreement
+    {
+        //Fields
+        private int ID;
+        private string slaDescription;
+        private string level_extent;
+
+        //Properties
+        public int ID1 { get => ID; set => ID = value; }
+        public string SlaDescription { get => slaDescription; set => slaDescription = value; }
+        public string Level_extent { get => level_extent; set => level_extent = value; }
+
+        //Custom COnstructor
+        public Service_Level_Agreement(int iD1, string slaDescription, string level_extent)
+        {
+            ID1 = iD1;
+            SlaDescription = slaDescription;
+            Level_extent = level_extent;
+        }
+          
+        public override bool Equals(object obj)
+        {
+
+            if ((obj == null) || !this.GetType().Equals(obj.GetType()))
+            {
+                return false;
+            }
+            else
+            {
+                Service_Level_Agreement p = (Service_Level_Agreement)obj;
+                return p.ID.Equals(this.ID);
+            }
+
+        }
+
+        public override int GetHashCode()
+        {
+            return this.ID;
+        } 
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+    }
+}
