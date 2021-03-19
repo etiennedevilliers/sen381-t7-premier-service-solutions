b88a4c1866f6ce154443342604f2ca3d1335adb7
RubenDebeer <boepsman@gmail.com>
2021-03-19 09:18:09 +0200

    AddedClients

---
+++ data/layer/objects/Clients/Client.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace sen381_t7_premier_service_solutions
+{
+    abstract class Client
+    {
+        protected List<Service_Contract> agreement;
+        protected string address;
+        protected string primary_contact_cell_number;
+
+        public abstract object client();
+            
+    }
+}
---
+++ data/layer/objects/Clients/Individual_Client.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace sen381_t7_premier_service_solutions
+{
+    class Individual_Client : Client
+    {
+        //Fields
+        private int ID;
+        private string name;
+        private string surname;
+
+        //Properties
+        public int ID1 { get => ID; set => ID = value; }
+        public string Name { get => name; set => name = value; }
+        public string Surname { get => surname; set => surname = value; }
+
+        public override object client()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if ((obj == null) || !this.GetType().Equals(obj.GetType()))
+            {
+                return false;
+            }
+            else
+            {
+                Individual_Client p = (Individual_Client)obj;
+                return p.ID.Equals(this.ID);
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return this.ID;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+    }
+}
