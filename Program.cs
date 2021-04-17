caeaf06e3555dc815d642c716fcf2be7fbfa0dff
Etienne de Villiers <etiennefdevilliers@gmail.com>
2021-04-17 21:06:11 +0200

    Added IChildren<Agent, Request> to RequestController

--- Program.cs
+++ Program.cs
@@ -9,34 +9,76 @@ namespace sen381_t7_premier_service_solutions
     {
         static void Main(string[] args)
         {
-            testRequests();
-
+            testAgents();
             Console.ReadLine();
         }
 
+        static void testAgents() {
+            AgentController agentController = new AgentController();
+            NewClientRequestController newClientRequestController = new NewClientRequestController();
+            CallLogController callLogController = new CallLogController();
+            RequestController requestController = new RequestController();
+
+            Agent kaylee = new Agent(
+                "kaylee",
+                "0605272390",
+                "unknown",
+                "unknown"
+            );
+
+            Agent johan = new Agent(
+                "kaylee",
+                "0605272390",
+                "unknown",
+                "unknown"
+            );
+
+            CallLog callLog = new CallLog(DateTime.Now, DateTime.Now, false);
+            NewClientRequest request = new NewClientRequest(DateTime.Now, DateTime.Now, callLog);
+
+            Console.WriteLine("Creating callLog...");
+            callLogController.Create(callLog);
+            Console.WriteLine("Creating newClientRequest...");
+            newClientRequestController.Create(request);
+            Console.WriteLine("Creating kaylee...");
+            agentController.Create(kaylee);
+            Console.WriteLine("Creating johan...");
+            agentController.Create(johan); // not added to any request
+            Console.WriteLine("Adding kaylee to request...");
+            requestController.Add(kaylee, request);
+            
+            Console.WriteLine("Reading children of request...");
+            foreach (Agent agent in requestController.ReadChildren(request))
+            {
+                Console.WriteLine(agent);
+            }
+        }
+
         static void testRequests() {
+            NewClientRequestController newClientRequestController = new NewClientRequestController();
+            NewContractRequestController newContractRequestController = new NewContractRequestController();
+            ComplaintRequestController complaintRequestController = new ComplaintRequestController();
+            ServiceRequestController serviceRequestController = new ServiceRequestController();
+            CallLogController callLogController = new CallLogController();
+
             Console.WriteLine("Setup new objects");
             CallLog callLog = new CallLog(DateTime.Now, DateTime.Now, false);
-            NewClientRequest newClientRequest = new NewClientRequest(DateTime.Now, DateTime.Now, callLog);
+            ServiceRequest serviceRequest = new ServiceRequest(DateTime.Now, DateTime.Now, callLog, "insert test", DateTime.Now);
 
-            Console.WriteLine("Creating callLog");
-            new CallLogController().Create(callLog);
-            NewClientRequestController newClientRequestController = new NewClientRequestController();
 
-            Console.WriteLine("Creating newClientRequest");
-            newClientRequestController.Create(newClientRequest);
+            Console.WriteLine("Creating new objects in Database");
+            callLogController.Create(callLog);
+            serviceRequestController.Create(serviceRequest);
 
-            Console.WriteLine("Creating ComplaintRequest");
-            ComplaintRequestController complaintRequestController = new ComplaintRequestController();
-            ComplaintRequest complaintRequest = new ComplaintRequest(DateTime.Now, DateTime.Now, callLog, "description!!!");
-            complaintRequestController.Create(complaintRequest);
-
+            
+            
 
             Console.WriteLine("Reading requests");
             List<Request> requests = new List<Request>();
             requests.AddRange(newClientRequestController.Read());
+            requests.AddRange(newContractRequestController.Read());
             requests.AddRange(complaintRequestController.Read());
-
+            requests.AddRange(serviceRequestController.Read());
 
             foreach (Request req in requests) {
                 Console.WriteLine(req);
