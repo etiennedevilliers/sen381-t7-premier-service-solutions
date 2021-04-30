2bd542c6dcd7299cf5039092e7eefbcbedd76ef4
etiennedevilliers <etiennefdevilliers@gmail.com>
2021-04-30 23:35:20 +0200

    Implemented IChildren<Employee, BusinessClient>

--- data/layer/controller/Clients/EmployeeController.cs
+++ data/layer/controller/Clients/EmployeeController.cs
@@ -6,68 +6,8 @@ using System.Data.SqlClient;
 
 namespace Data.Layer.Controller
 {
-    class EmployeeController : IRead<Employee>, ICreate<Employee>, IUpdate<Employee>, IDelete<Employee>
+    class EmployeeController : IUpdate<Employee>
     {
-        BusinessClient bus;
-
-        public EmployeeController(BusinessClient bus)
-        {
-            this.bus = bus;
-        }
-
-        //Basic CRUD
-        public int Create(Employee obj)
-        {
-            DataHandler dh = new DataHandler();
-
-            obj.Id = dh.InsertID(string.Format(
-                    "INSERT INTO Employee(name, surname, role, contactNum, BusinessClientID) VALUES ('{0}', '{1}', '{2}', '{3}', '{4}')",
-                    obj.Name, obj.Surname, obj.Role, obj.ContactNum, bus.Id));
-
-            dh.Dispose();
-
-            return obj.Id;
-        }
-
-        public void Delete(Employee obj)
-        {
-            DataHandler dh = new DataHandler();
-
-            dh.Delete("Employee", "EmployeeID = " + obj.Id.ToString());
-
-            dh.Dispose();
-        }
-
-        public List<Employee> Read()
-        {
-            DataHandler dh = new DataHandler();
-
-            List<Employee> empList = new List<Employee>();
-            SqlDataReader read = dh.Select("SELECT EmployeeID, name, surname, role, contactNum FROM Employee WHERE BusinessClientID = " + bus.Id.ToString());
-            Employee newEmp;
-
-            if (read.HasRows)
-            {
-                while (read.Read())
-                {
-                    newEmp = new Employee(
-                            read.GetString(1),
-                            read.GetString(2),
-                            read.GetString(3),
-                            read.GetString(4)
-                        );
-
-                    newEmp.Id = read.GetInt32(0);
-
-                    empList.Add(newEmp);
-                }
-            }
-
-            dh.Dispose();
-
-            return empList;
-        }
-
         public void Update(Employee obj)
         {
             DataHandler dh = new DataHandler();
